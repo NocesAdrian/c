31724aba135420e143ae403339cc07e4efc89abf
Adrian Noces <adriannoces1@gmail.com>
2025-03-02 11:16:22 +0000

    m

---
+++ Libraries/01stdio.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    // Input & Output Functions
+    printf(); // Prints formatted output to the console
+    scanf(); // Reads formatted input from the user
+    puts(); // Prints a string followed by a newline.
+    fgets(); // Reads a string from input
+
+    // File Handling Functions
+    fopen(); // Opens a file
+    fclose(); // Closes an open file
+    fgets(); // Reads a line from a file
+    fputs(); // Writes a string to a file
+    fscanf(); // Reads formatted input from a file
+    fprintf(); // Writes formatted output to a file
+
+    // Character Input & Output
+    getchar(); // Reads a single character from input
+    putchar(); // Prints a single character
+    fgetc(); // Reads a character from a file
+    fputc(): // Writes a character to a file
+
+    // File Positioning Functions
+    fseek(); // Moves file pointer to a specific position
+    ftell(); // Returns the current position in a file
+    rewind(); // Moves file pointer to the beginning
+}
