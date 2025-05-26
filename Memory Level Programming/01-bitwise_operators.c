a277803e66df236b12f26580d069ccac90d98195
NocesAdrian <adriannoces1@gmail.com>
2025-05-26 20:33:09 +0800

    upd

---
+++ Memory Level Programming/01-bitwise_operators.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    // BITWISE OPERATOR = special operator used in bit level programming
+    //                    (Knowing binary is important to this topic)
+
+    // & = AND
+    // | = OR
+    // ^ = XOR
+    // << left shift
+    // >> right shift
+    
+    int x = 6;      // 00000110 bit > 6
+    int y = 12;     // 00001100 bit > 12
+    int z = 0;      // 00000000 bit > 0
+
+    z = x & y;      // 00000100 bit > 4
+    printf("x & y = %d\n", z);
+
+    z = x | y;      // 00001110 bit > 14;
+    printf("x | y = %d\n", z);
+
+    z = x ^ y;      // 00001010 bit > 10
+    printf("x ^ y = %d\n", z);
+
+    z = x << 1;     // 00001100 bit > 12
+    printf("x << 1 = %d\n", z);
+
+    z = y >> 1;     // 00000110 bit > 6
+    printf("y >> 1 = %d\n", z);
+}
---
+++ RealApplications/02-QUIZ-GAME.c
@@ -0,0 +1,89 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main() {
+    
+    // Questions
+    char questions[][50] = {
+        "1. What is the capital of Japan?",
+        "2. Who developed the theory of relativity?",
+        "3. What is the binary value of 5?"
+    };
+
+    // Answers to each Questions
+    char answers[3] = {
+        'B', 
+        'C', 
+        'A'
+    };
+
+    // Choices for each question
+    char choices[][20] = {
+        "A. Seoul", "B. Tokyo", "C. Beijing", "D. Kyoto",
+        "A. Newton", "B. Tesla", "C. Einstein", "D. Bohr",
+        "A. 101", "B. 110", "C. 100", "D. 111"
+    };
+
+    
+
+    int sizeOfQuestions = sizeof(questions)/sizeof(questions[0]);
+    int sizeOfchoices = sizeof(choices)/sizeof(choices[0]);
+    int sizeOFAnswers = sizeof(answers)/sizeof(answers[0]);
+
+    char History[sizeOfQuestions];
+    char PlayerAnswer;
+    int score = 0;
+
+    char buffer[10];
+
+
+    for (int i = 0; i < sizeOfQuestions; i++) {
+        
+        printf("###################################################\n");
+        
+        printf("%s: \n", questions[i]);
+        
+        // good practice for where the iteration begin and limition to increment
+        for (int j = (i * 4); j < (i * 4) + 4; j++) {
+            printf("%s\n", choices[j]);
+        }
+
+        printf("###################################################\n");
+        
+        printf("Your Answer: ");
+        // scanf("%c", &PlayerAnswer);
+        // getchar();
+ 
+        fgets(buffer, sizeof(buffer), stdin);
+        // store the first element of array of buffer which is a character to PlayerAnswer
+        PlayerAnswer = toupper(buffer[0]);
+    
+        History[i] = PlayerAnswer;
+
+        if (PlayerAnswer == answers[i]) {
+            score++;
+            printf("CORRECT!\n\n");
+        } else if(PlayerAnswer > 68 || PlayerAnswer < 65) {
+            printf("%c is not in the choices A - D\n\n", PlayerAnswer);
+            i--;
+        } else {
+            printf("WRONG!\n\n");
+        }
+        
+    }
+
+    printf("#######################\n");
+    printf("[FINISHED]\n");
+    printf("score: %d/%d\n", score, sizeOfQuestions);
+    printf("#######################\n\n");
+
+    printf("SUMMARY \n");
+    for (int i = 0; i < sizeOfQuestions; i++) {
+        printf("###################################################\n");
+        printf("%s\n", questions[i]);
+        printf("your answer: %c\n", History[i]);
+        printf("Correct answer: %c\n", answers[i]);
+        printf("###################################################\n\n");
+    }
+
+}
