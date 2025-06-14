02642a362476b336c16fd75ffe43e147b1b4cfd4
NocesAdrian <adriannoces1@gmail.com>
2025-06-14 20:54:32 +0800

    upd

---
+++ FileManagement/fetchCharactersDynamically.c
@@ -0,0 +1,53 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// user defined function
+int fetch_characters_from_file_to_memory(char *, char **, size_t *);
+
+int main() {
+
+    // count of maximum characters that the memory can store
+    size_t max_count = 128;
+    // allocate memory
+    char *array_of_characters = (char *)malloc(max_count * sizeof(char));   // can store 128 characters
+    // error handling for allocatig memory
+    if (!array_of_characters) return 1;
+
+    // using user defined function with error handling
+    if (fetch_characters_from_file_to_memory("src/test.txt", &array_of_characters, &max_count) == 1) {
+        free(array_of_characters);
+        return 1; 
+    }
+
+    return 0;
+}
+
+// user defined function
+int fetch_characters_from_file_to_memory(char *filename, char **array, size_t *max_count) {
+
+    // open and read file
+    FILE *fp = fopen(filename, "r");
+    
+    // error handling
+    if(fp == NULL) { fprintf(stderr, "failed to open %s\n", filename); return 1; }
+
+    int buffer;
+    size_t index = 0;
+    while ((buffer = fgetc(fp)) != EOF) {
+        if (index >= *max_count) {
+            *max_count *= 2;
+            char *tmp = (char *)realloc(*array, (*max_count) * sizeof(char));
+            if (!tmp) {fclose(fp); return 1;}
+            *array = tmp;
+        }
+        (*array)[index] = (char)buffer;
+        index++;
+    }
+
+    (*array)[index] = '\0';
+    printf("%s", *array);
+
+    fclose(fp);
+    return 0;
+}
